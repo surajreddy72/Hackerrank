98441941511395770204718ab229a6e9273f420d
Suraj Reddy <82399778+surajreddy72@users.noreply.github.com>
2022-07-10 02:26:41 +0530

    Update Multiples of 3 and 5.py

--- Project Euler +/Multiples of 3 and 5.py
+++ Project Euler +/Multiples of 3 and 5.py
@@ -13,21 +13,3 @@ for j in list:
     n3 = int(i)//15
     sum = (3*n1*(n1+1)//2) + (5*n2*(n2+1)//2) - (15*n3*(n3+1)//2)
     print(int(sum))
-
-
-# 60 points for this 
-
-t = int(input())
-list = []
-for _ in range(t):
-    n=input()
-    list.append(n)
-for i in list:
-    sum = 0
-    a = 1
-    while a < int(i) :
-        if a%3==0 or a%5==0 :
-            sum = sum + a
-        a = a + 1
-    print(sum)
-# Since time complexity is more
